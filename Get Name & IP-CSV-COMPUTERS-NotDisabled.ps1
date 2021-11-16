0e154135e6fd6eab13850ed435f356d2e0fc1431
Zacvr <58831135+Zacvr@users.noreply.github.com>
2021-11-16 07:56:28 -0800

    Add files via upload

---
+++ Get Name & IP-CSV-COMPUTERS-NotDisabled.ps1
@@ -0,0 +1,5 @@
+﻿$RemoteComputer = Read-Host "Computer Name?"
+$RemoteComputer = $RemoteComputer.ToUpper()
+#Get-ADComputer -Filter ('Name -like "*' + $RemoteComputer + '*"') -Properties IPv4Address | Sort-Object Name | FT Name,IPv4Address -A | Export-CSV $PSScriptRoot\$RemoteComputer.CSV
+#Get-ADComputer -Filter ('Name -like "*' + $RemoteComputer + '*"') -Properties IPv4Address | Sort-Object Name | FT Name,IPv4Address -A | Export-CSV $PSScriptRoot\$RemoteComputer.CSV
+Get-ADComputer -Filter ('Name -like "*' + $RemoteComputer + '*"')  -Property * | Where-Object Distinguishedname -Match 'ou=COMPUTERS' | Where-Object Distinguishedname -Notmatch 'ou=disabled' | Where-Object Distinguishedname -Notmatch 'ou=DO NOT USE OU_Domain_Root' |Sort-Object Name | Select-Object Name,ipv4Address,OperatingSystem,OperatingSystemVersion | Export-CSV $PSScriptRoot\$RemoteComputer.CSV -NoTypeInformation -Encoding UTF8
